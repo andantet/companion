ec4f037ea6b35d6ef4223d56496a8c1483346000
andantet <orrdaniel73@gmail.com>
2022-10-05 08:37:14 +0100

    empty slot highlights fix

--- mccic-qol/src/main/java/dev/andante/mccic/qol/client/config/QolClientConfig.java
+++ mccic-qol/src/main/java/dev/andante/mccic/qol/client/config/QolClientConfig.java
@@ -8,14 +8,20 @@ import net.fabricmc.api.Environment;
 import net.minecraft.util.StringIdentifiable;
 
 @Environment(EnvType.CLIENT)
-public record QolClientConfig(ConfirmDisconnectMode confirmDisconnectMode) {
+public record QolClientConfig(ConfirmDisconnectMode confirmDisconnectMode, boolean emptySlotHighlightsFix) {
     public static final Codec<QolClientConfig> CODEC = RecordCodecBuilder.create(
-        instance -> instance.group(
-            StringIdentifiable.createCodec(ConfirmDisconnectMode::values)
-                              .fieldOf("confirm_disconnect_mode")
-                              .orElse(createDefaultConfig().confirmDisconnectMode())
-                              .forGetter(QolClientConfig::confirmDisconnectMode)
-        ).apply(instance, QolClientConfig::new)
+        instance -> {
+            QolClientConfig defaultConfig = createDefaultConfig();
+            return instance.group(
+                StringIdentifiable.createCodec(ConfirmDisconnectMode::values)
+                                  .fieldOf("confirm_disconnect_mode")
+                                  .orElse(defaultConfig.confirmDisconnectMode())
+                                  .forGetter(QolClientConfig::confirmDisconnectMode),
+                Codec.BOOL.fieldOf("empty_slot_highlights_fix")
+                          .orElse(defaultConfig.emptySlotHighlightsFix())
+                          .forGetter(QolClientConfig::emptySlotHighlightsFix)
+            ).apply(instance, QolClientConfig::new);
+        }
     );
 
     public static final ConfigHolder<QolClientConfig> CONFIG_HOLDER = new ConfigHolder<>("qol", CODEC, createDefaultConfig());
@@ -25,6 +31,6 @@ public record QolClientConfig(ConfirmDisconnectMode confirmDisconnectMode) {
     }
 
     public static QolClientConfig createDefaultConfig() {
-        return new QolClientConfig(ConfirmDisconnectMode.IN_GAME);
+        return new QolClientConfig(ConfirmDisconnectMode.IN_GAME, true);
     }
 }
---
+++ mccic-qol/src/main/java/dev/andante/mccic/qol/mixin/client/HandledScreenMixin.java
@@ -0,0 +1,55 @@
+package dev.andante.mccic.qol.mixin.client;
+
+import dev.andante.mccic.api.client.game.GameTracker;
+import dev.andante.mccic.api.game.GameState;
+import dev.andante.mccic.qol.client.config.QolClientConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.screen.slot.Slot;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(HandledScreen.class)
+public class HandledScreenMixin {
+    @Shadow @Nullable protected Slot focusedSlot;
+    @Unique private static Slot staticFocusedSlot;
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        staticFocusedSlot = null;
+    }
+
+    @Inject(
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;drawSlotHighlight(Lnet/minecraft/client/util/math/MatrixStack;III)V"
+        )
+    )
+    private void onRenderDrawSlotHighlight(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        staticFocusedSlot = this.focusedSlot;
+    }
+
+    /**
+     * Cancels rendering of slot highlight when on MCCI, not in-game, configured and slot is empty.
+     */
+    @Inject(method = "drawSlotHighlight", at = @At("HEAD"), cancellable = true)
+    private static void onRenderDrawSlotHighlight(MatrixStack matrices, int x, int y, int z, CallbackInfo ci) {
+        GameTracker tracker = GameTracker.INSTANCE;
+        if (tracker.isOnServer() && tracker.getGameState() != GameState.ACTIVE) {
+            if (staticFocusedSlot != null) {
+                if (QolClientConfig.getConfig().emptySlotHighlightsFix() && !staticFocusedSlot.hasStack()) {
+                    ci.cancel();
+                }
+            }
+        }
+    }
+}
