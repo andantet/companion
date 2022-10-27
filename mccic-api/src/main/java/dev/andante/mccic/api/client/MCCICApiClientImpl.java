ffa18baa0bc8f278d93c184a714716a7e5b65625
andantet <orrdaniel73@gmail.com>
2022-10-27 11:51:27 +0100

    move american date format builtin resource pack from qol -> api, update icon

--- mccic-api/src/main/java/dev/andante/mccic/api/client/MCCICApiClientImpl.java
+++ mccic-api/src/main/java/dev/andante/mccic/api/client/MCCICApiClientImpl.java
@@ -6,6 +6,10 @@ import dev.andante.mccic.api.client.game.GameTracker;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
 
 @SuppressWarnings("UnstableApiUsage")
 @Environment(EnvType.CLIENT)
@@ -13,6 +17,12 @@ public final class MCCICApiClientImpl implements MCCICApi, ClientModInitializer 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Initializing {}", MOD_NAME);
+
         Reflection.initialize(GameTracker.class, UpdateTracker.class);
+
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(container -> {
+            Identifier id = new Identifier(container.getMetadata().getId(), "american_date_format");
+            ResourceManagerHelper.registerBuiltinResourcePack(id, container, "MCCIC: American Date Format", ResourcePackActivationType.NORMAL);
+        });
     }
 }
--- mccic-qol/src/main/java/dev/andante/mccic/qol/client/MCCICQolClientImpl.java
+++ mccic-qol/src/main/java/dev/andante/mccic/qol/client/MCCICQolClientImpl.java
@@ -8,10 +8,6 @@ import dev.andante.mccic.qol.client.config.QoLConfigScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public final class MCCICQolClientImpl implements MCCICQoL, ClientModInitializer {
@@ -20,10 +16,5 @@ public final class MCCICQolClientImpl implements MCCICQoL, ClientModInitializer 
     public void onInitializeClient() {
         ClientConfigRegistry.INSTANCE.registerAndLoad(QoLClientConfig.CONFIG_HOLDER, QoLConfigScreen::new);
         MCCICConfigCommand.registerNewConfig(ID, QoLConfigScreen::new);
-
-        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(container -> {
-            Identifier id = new Identifier(container.getMetadata().getId(), "american_date_format");
-            ResourceManagerHelper.registerBuiltinResourcePack(id, container, "MCCIC: American Date Format", ResourcePackActivationType.NORMAL);
-        });
     }
 }
