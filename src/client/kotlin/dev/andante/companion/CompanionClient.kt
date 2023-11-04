accc687c0f23079e85b353d79b4ce5762b0c5c0a
Daniel Orr <orrdaniel73@gmail.com>
2023-11-04 10:36:07 +0000

    Initial commit

---
+++ src/client/kotlin/dev/andante/companion/CompanionClient.kt
@@ -0,0 +1,9 @@
+package dev.andante.companion
+
+import net.fabricmc.api.ClientModInitializer
+
+object CompanionClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/dev/andante/companion/Companion.kt
@@ -0,0 +1,15 @@
+package dev.andante.companion
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Companion : ModInitializer {
+    private val logger = LoggerFactory.getLogger("companion")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
